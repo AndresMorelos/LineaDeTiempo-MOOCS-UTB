ed7a6635f99975ecad1181d182e2e2236719381b
Andres Morelos <andresmorelos123@gmail.com>
2017-09-22 10:10:36 -0500

    Optimización codigo TypeScript.

--- js/main.js
+++ js/main.js
@@ -4,24 +4,32 @@ var fecha_2000_2025 = document.getElementsByClassName('contenedor-der')[0];
 var contenedor_info_anos_izq = document.getElementsByClassName('contenedor-info-anos-izq')[0];
 var contenedor_info_anos_der = document.getElementsByClassName('contenedor-info-anos-der')[0];
 var regresar_button = document.getElementById('regresar-button');
+var level1_ocultos = [fecha_1970_1977, fecha_2000_2025];
+var level1_izquierda_mostrados = [contenedor_info_anos_izq, regresar_button];
+var level1_derecha_mostrados = [contenedor_info_anos_der, regresar_button];
 var div_anterior_oculto = [];
 var div_mostrados = [];
+function assig_level_data_hide(data) {
+    div_anterior_oculto = data;
+}
+function assig_level_data_show(data) {
+    div_mostrados = data;
+}
 function mostrar_contenido_level1() {
     fecha_1970_1977.classList.add("display-none");
     fecha_2000_2025.classList.add("display-none");
+    assig_level_data_hide(level1_ocultos);
     regresar_button.classList.remove("display-none");
 }
 function mostrar_contenido_izquierda() {
     mostrar_contenido_level1();
     contenedor_info_anos_izq.classList.remove("display-none");
-    div_anterior_oculto.push(fecha_1970_1977, fecha_2000_2025);
-    div_mostrados.push(contenedor_info_anos_izq, regresar_button);
+    assig_level_data_show(level1_izquierda_mostrados);
 }
 function mostrar_contenido_derecha() {
     mostrar_contenido_level1();
     contenedor_info_anos_der.classList.remove("display-none");
-    div_anterior_oculto.push(fecha_1970_1977, fecha_2000_2025);
-    div_mostrados.push(contenedor_info_anos_der, regresar_button);
+    assig_level_data_show(level1_derecha_mostrados);
 }
 function regresar() {
     for (var _i = 0, div_mostrados_1 = div_mostrados; _i < div_mostrados_1.length; _i++) {
@@ -32,8 +40,6 @@ function regresar() {
         var div_oculto = div_anterior_oculto_1[_a];
         div_oculto.classList.remove("display-none");
     }
-    div_anterior_oculto.splice(0, div_anterior_oculto.length);
-    div_mostrados.splice(0, div_mostrados.length);
 }
 //Para cuando se tenga la opción de mouseover
 fecha_1970_1977.onmouseover = function () {
--- js/main.ts
+++ js/main.ts
@@ -4,27 +4,36 @@ let fecha_2000_2025:HTMLElement = <HTMLElement> document.getElementsByClassName(
 let contenedor_info_anos_izq:HTMLElement = <HTMLElement> document.getElementsByClassName('contenedor-info-anos-izq')[0];
 let contenedor_info_anos_der:HTMLElement = <HTMLElement> document.getElementsByClassName('contenedor-info-anos-der')[0];
 let regresar_button:HTMLElement = <HTMLElement> document.getElementById('regresar-button');
+let level1_ocultos:HTMLElement[] = [fecha_1970_1977, fecha_2000_2025];
+let level1_izquierda_mostrados:HTMLElement[] = [contenedor_info_anos_izq, regresar_button];
+let level1_derecha_mostrados:HTMLElement[] = [contenedor_info_anos_der, regresar_button];
 let div_anterior_oculto:HTMLElement[] = [];
 let div_mostrados:HTMLElement[] = [];
+
+function assig_level_data_hide(data:HTMLElement[]){
+  div_anterior_oculto = data;
+}
+function assig_level_data_show(data:HTMLElement[]){
+  div_mostrados = data;
+}
 
 function mostrar_contenido_level1(){
   fecha_1970_1977.classList.add("display-none");
   fecha_2000_2025.classList.add("display-none");
+  assig_level_data_hide(level1_ocultos);
   regresar_button.classList.remove("display-none");
 }
 
 function mostrar_contenido_izquierda(){
   mostrar_contenido_level1()
   contenedor_info_anos_izq.classList.remove("display-none");
-  div_anterior_oculto.push(fecha_1970_1977, fecha_2000_2025);
-  div_mostrados.push(contenedor_info_anos_izq,regresar_button);
+  assig_level_data_show(level1_izquierda_mostrados);
 }
 
 function mostrar_contenido_derecha(){
   mostrar_contenido_level1()
   contenedor_info_anos_der.classList.remove("display-none");
-  div_anterior_oculto.push(fecha_1970_1977, fecha_2000_2025);
-  div_mostrados.push(contenedor_info_anos_der,regresar_button);
+  assig_level_data_show(level1_derecha_mostrados);
 }
 
 function regresar(){
@@ -35,8 +44,6 @@ function regresar(){
   for (let div_oculto of div_anterior_oculto) {
       div_oculto.classList.remove("display-none");
   }
-  div_anterior_oculto.splice(0,div_anterior_oculto.length);
-  div_mostrados.splice(0,div_mostrados.length);
 }
 
 //Para cuando se tenga la opción de mouseover
